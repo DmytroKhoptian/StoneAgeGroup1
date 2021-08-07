69ee64d86aa88ee5029591a4ac90123825bb4b7f
DmytroKhoptian <48955602+DmytroKhoptian@users.noreply.github.com>
2021-08-07 12:58:50 +0300

    PlayerControl

--- Stone Age Group1/Assets/Scripts/PlayerMovement.cs
+++ Stone Age Group1/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float playerSpeed;
     [SerializeField] private float playerJumpForce;
 
+    [SerializeField] private GameObject animObject;
+
+    private SpriteRenderer spriteRenderer;
+
     private float currentPlayerSpeed;
     private Rigidbody2D rb;
     private bool groundCheck;
@@ -16,32 +20,53 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = animObject.GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
         //ДЗ добавить enum
-        rb.velocity = new Vector2(currentPlayerSpeed * Time.deltaTime, rb.velocity.y); //Time.deltaTime??
+        rb.velocity = new Vector2(currentPlayerSpeed*Time.deltaTime, rb.velocity.y);  
+        //rb.AddForce(new Vector2(currentPlayerSpeed, 0f), ForceMode2D.Force);
     }
-
-
 
     public void RightMove()
     {
         currentPlayerSpeed = playerSpeed;
-        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        spriteRenderer.flipX = false; 
     }
     public void LeftMove()
     {
         currentPlayerSpeed = -playerSpeed;
-        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.y);
+        spriteRenderer.flipX = true;    
     }
+
+ 
     public void StopMove()
     {
         currentPlayerSpeed = 0f;
     }
     public void Jump()
     {
-
+        if (groundCheck)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, playerJumpForce);
+            groundCheck = false;
+        }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        groundCheck = true;
+    }
+
 }
+
+
+
+
+
+
+//if (transform.localScale.x > 0)
+//{
+//    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.y);
+//}
